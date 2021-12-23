d5eab98a5096a778845fae065cae1e0484f81549
filip <fikaaaa14@gmail.com>
2021-12-23 14:55:33 +0100

    dodavanje na branch

---
+++ src/main/java/com/academy/demo/config/FakeApiConfiguration.java
@@ -0,0 +1,52 @@
+package com.academy.demo.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "fake-store")
+public class FakeApiConfiguration {
+
+    private String url;
+    private String productPart;
+    private String categoryPart;
+    private String cartPart;
+
+    @Bean
+    public FakeApiConfiguration fakeApiConfiguration(){
+        return new FakeApiConfiguration();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getProductPart() {
+        return productPart;
+    }
+
+    public void setProductPart(String productPart) {
+        this.productPart = productPart;
+    }
+
+    public String getCategoryPart() {
+        return categoryPart;
+    }
+
+    public void setCategoryPart(String categoryPart) {
+        this.categoryPart = categoryPart;
+    }
+
+    public String getCartPart() {
+        return cartPart;
+    }
+
+    public void setCartPart(String cartPart) {
+        this.cartPart = cartPart;
+    }
+}
--- src/main/java/com/academy/demo/service/ProductFakerService.java
+++ src/main/java/com/academy/demo/service/ProductFakerService.java
@@ -1,9 +1,18 @@
 package com.academy.demo.service;
 
+import com.academy.demo.config.FakeApiConfiguration;
+import com.academy.demo.dto.CartDTO;
 import com.academy.demo.dto.ProductDTO;
+import com.academy.demo.dto.ProductFakeDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +23,35 @@ import java.util.stream.Collectors;
 public class ProductFakerService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProductFakerService.class);
+
+    @Autowired
+    private FakeApiConfiguration fakeApiConfiguration;
+
+    @Qualifier("secureRestTemplate")
+    private RestTemplate secureRestTemplate;
+
+    /*public void config() {
+        String url = fakeApiConfiguration.getUrl();
+        String productPart = fakeApiConfiguration.getProductPart();
+        String categoryPart = fakeApiConfiguration.getCategoryPart();
+        String cartPart = fakeApiConfiguration.getCartPart();
+    }*/
+
+    public ProductFakeDTO addNewProduct(ProductFakeDTO productFakeDTO) {
+
+        String url = fakeApiConfiguration.getUrl() + fakeApiConfiguration.getProductPart();
+
+        try {
+            ResponseEntity<ProductFakeDTO> responseEntity = secureRestTemplate.exchange(url, HttpMethod.POST, null, ProductFakeDTO.class);
+            return responseEntity.getBody();
+        }
+
+        catch (Exception e) {
+            return null;
+        }
+
+
+    }
 
     public List<ProductDTO> findAll()
     {
