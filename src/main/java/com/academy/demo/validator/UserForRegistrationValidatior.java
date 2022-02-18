8426fd2a7de5a07d7e3c4f32c8f0efcec6e4c9cb
filip <fikaaaa14@gmail.com>
2022-02-18 22:37:20 +0100

    Bean dodat na klasi validator

--- src/main/java/com/academy/demo/validator/UserForRegistrationValidatior.java
+++ src/main/java/com/academy/demo/validator/UserForRegistrationValidatior.java
@@ -1,9 +1,11 @@
 package com.academy.demo.validator;
 
 import com.academy.demo.security.dto.UserForRegistrationDTO;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+@Component
 public class UserForRegistrationValidatior implements Validator {
 
     @Override
